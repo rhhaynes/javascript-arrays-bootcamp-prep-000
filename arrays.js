8db84e561123ace33740f422505e624ed9954977
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-04 17:08:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,17 @@
 function addElementToBeginningOfArray(array,element2add){
-  //text
+  var newArray = [element2add,...array]
+  return newArray
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element2add){
+  array = [element2add,...array]
+  return array
+}
+
+function addElementToEndOfArray(){
+  //text
+}
+
+function destructivelyAddElementToEndOfArray(){
   //text
 }
